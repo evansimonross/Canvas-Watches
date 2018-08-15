14a772f6391ba165f4764283a11cb5410a0998eb
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-08-14 23:09:57 -0400

    convert.js now gets filename of xml file from command line.

--- assets/javascript/convert.js
+++ assets/javascript/convert.js
@@ -2,30 +2,37 @@ var convert = require('xml-js');
 var parser = require('luaparse');
 var fs = require('fs');
 
-var xmlWatch = require('fs').readFileSync('example.xml', 'utf8');
-var options = { compact: true, ignoreComment: true, alwaysChildren: true };
-var jsWatch = convert.xml2js(xmlWatch, options);
-
 var interpret = (lua) => {
     var ast = parser.parse(lua);
     return ast;
 }
 
-var parse = () => {
-    var layers = jsWatch.Watch.Layer;
-    for (var i = 0; i < layers.length; i++) {
-        var layer = layers[i];
-        for (attribute in layer['_attributes']) {
-            if (layer['_attributes'][attribute].search(/\+|\-|\*|\/|\<|\>|\=/) > -1) {
-                layer['_attributes'][attribute] = interpret(attribute + '=' + layer['_attributes'][attribute])
+var getWatchFromXml = (xmlName) => {
+    if(xmlName.indexOf('.xml')>-1){
+        xmlName = xmlName.substring(0,xmlName.indexOf('.xml'));
+    }
+    var xmlWatch = require('fs').readFileSync(xmlName + '.xml', 'utf8');
+    var options = { compact: true, ignoreComment: true, alwaysChildren: true };
+    var jsWatch = convert.xml2js(xmlWatch, options);
+
+    var parse = () => {
+        var layers = jsWatch.Watch.Layer;
+        for (var i = 0; i < layers.length; i++) {
+            var layer = layers[i];
+            for (attribute in layer['_attributes']) {
+                if (layer['_attributes'][attribute].search(/\+|\-|\*|\/|\<|\>|\=/) > -1) {
+                    layer['_attributes'][attribute] = interpret(attribute + '=' + layer['_attributes'][attribute])
+                }
             }
         }
+        jsWatch.Watch.Layer = layers;
+        fs.writeFile(xmlName + '.js', 'var watch = ' + JSON.stringify(jsWatch, null, 2), (err) => {
+            if (err) { console.error(err) }
+            console.log('Saved')
+        })
     }
-    jsWatch.Watch.Layer = layers;
-    fs.writeFile('example.js', 'var watch = ' + JSON.stringify(jsWatch, null, 2), (err) => {
-        if (err) { console.error(err) }
-        console.log('Saved')
-    })
+
+    parse();
 }
 
-parse()
+getWatchFromXml(process.argv[2]);

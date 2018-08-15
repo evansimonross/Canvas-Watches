2d29a54bdab5c9f7af177b865465ac9b0a377c90
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-08-14 23:47:02 -0400

    Added Title to watch

--- assets/javascript/display.js
+++ assets/javascript/display.js
@@ -1,10 +1,11 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var radius = canvas.height / 2;
-ctx.translate(radius, radius);
+document.getElementById("watchName").textContent = (watch.Watch['_attributes'].name)
 
 var drawWatch = () => {
-    ctx.arc(0, 0, radius, 0 , 2*Math.PI);
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    var radius = canvas.height / 2;
+    ctx.translate(radius, radius);
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
     ctx.fillStyle = "black";
     ctx.fill();
     console.log(watch)
